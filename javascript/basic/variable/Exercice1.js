01658c12463f48e0aebbc23dec35139f21419147
ASMA151200 <baasma1222@gmail.com>
2026-01-30 23:46:20 +0000

    Add variable exercise and correction in Exercice1.js
    Added exercise instructions and corrected code for variable declaration.

--- javascript/basic/variable/Exercice1.js
+++ javascript/basic/variable/Exercice1.js
@@ -1,1 +1,15 @@
+### exercice1 les variable
+-Déclarons deux variables : et .adminname
+-Attribuons la valeur à ."John"name
+-Copiez la valeur de vers .nameadmin
+-Montrer la valeur d’utiliser (doit produire « John »).adminalert
 
+### correction:
+
+let admin, name;
+
+name = "Jonh";
+
+admin = name;
+
+alert( admin);
