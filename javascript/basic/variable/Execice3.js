5ce35f02b8b0b8ac28c05845730ba485459887d1
ASMA151200 <baasma1222@gmail.com>
2026-01-30 23:48:12 +0000

    Add example for const variable usage in Execice3.js
    Added instructions and example for using const variable.

--- javascript/basic/variable/Execice3.js
+++ javascript/basic/variable/Execice3.js
@@ -1,1 +1,16 @@
+Objectif : comprendre la différence
 
+Consigne :
+
+- Crée une variable pays avec const
+
+- Affiche-la
+
+- Essaie de la modifier (juste pour voir l’erreur)
+
+const pays='France';
+
+//pays='Maroc';
+
+alert(pays);
+//Rien ne va s'afficher parceque quand on declare une valeur avec la variable const elle reste constante
