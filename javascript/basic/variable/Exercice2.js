5c9dcacbe8a69edcfa1979e2c50fee579e35c836
ASMA151200 <baasma1222@gmail.com>
2026-01-30 23:46:55 +0000

    Update Exercice2.js to show variable change
    Demonstrate variable reassignment with age.

--- javascript/basic/variable/Exercice2.js
+++ javascript/basic/variable/Exercice2.js
@@ -1,1 +1,14 @@
+Objectif : voir qu’une variable peut changer
 
+Consigne :
+
+1/Crée une variable age
+2/Mets un nombre
+3/Change la valeur
+4/Affiche-la
+
+let age= 18;
+
+ age= 20;
+
+alert(age);
