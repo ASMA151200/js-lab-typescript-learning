f4c3a9729da06a080726b83c09009b69be5b093f
ASMA151200 <baasma1222@gmail.com>
2026-01-30 23:49:27 +0000

    Implement message display with name and age

--- javascript/basic/variable/Exercice4.js
+++ javascript/basic/variable/Exercice4.js
@@ -1,1 +1,18 @@
+Objectif : afficher un message
+
+Consigne :
+
+Crée prenom
+
+Crée message
+
+Affiche : je m'appelle Asma et j'ai 21 ans 
+
+let prenom = "Asma";
+let age = 21;
+
+
+let message ="je m'appelle  " + prenom + " et " + " j" + "'"+"ai " + age + " ans" ;
+
+alert(message);
 
